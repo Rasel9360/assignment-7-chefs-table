bdaa0d9099d2958f8e933ed4681d8cd04afe5dfd
Rasel9360 <raselmahamud9360@gmail.com>
2024-03-15 23:55:31 +0600

    Recipe card added

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,15 +1,17 @@
-import { FaClock } from "react-icons/fa6";
+import { CiClock2 } from "react-icons/ci";
+import Icon from '../../assets/image/Frame2.svg'
 
 const Recipe = () => {
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-xl border-2">
                 <figure className="px-10 pt-10">
                     <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">Spaghetti Bolognese</h2>
                     <p>Classic Italian pasta dish with savory meat sauce.</p>
+                    <hr />
                     <ul className="list-disc">
                         <li>500g ground beef</li>
                         <li>500g ground beef</li>
@@ -18,11 +20,12 @@ const Recipe = () => {
                         <li>500g ground beef</li>
                         <li>500g ground beef</li>
                     </ul>
-                    <div>
-                        <p><FaClock></FaClock> </p>
+                    <div className="flex justify-between">
+                        <p className="flex items-center gap-2"><CiClock2></CiClock2> 30 minutes</p>
+                        <p className="flex items-center gap-2"><img src={Icon} alt="" /> 30 minutes</p>
                     </div>
                     <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-success rounded-full">Want to Cook</button>
                     </div>
                 </div>
             </div>
